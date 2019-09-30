1aa2c6dabaea826e5c0a61920abc112ed77b5478
Abdallah <abdallahomar1992@gmail.com>
2019-09-30 15:11:55 +0300

    Animation

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+AnimationDrawable wifiAnimation;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView img=(ImageView)findViewById(R.id.img);
+        img.setBackgroundResource(R.drawable.animation);
+        wifiAnimation=(AnimationDrawable)img.getBackground();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        wifiAnimation.start();
+    }
+}
